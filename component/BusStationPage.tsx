27500b8ec898aae4e4ebd4102c7ecd890efd1fe1
ruubyme <wngml1741@naver.com>
2024-02-20 19:39:33 +0900

    CSS: loading spinner 가운데 정렬

--- component/BusStationPage.tsx
+++ component/BusStationPage.tsx
@@ -7,6 +7,7 @@ import {
 } from "../api";
 import { Station } from "../src/type";
 import { BusArriveList } from "./BusArriveList";
+import { useEffect } from "react";
 
 export const BusStationPage: React.FC = () => {
   const location = useLocation();
@@ -53,6 +54,10 @@ export const BusStationPage: React.FC = () => {
     { enabled: !!busStationSeqList }
   );
 
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div>
       <div className="h-screen">
@@ -63,7 +68,9 @@ export const BusStationPage: React.FC = () => {
         {isLoadingBusRoute ||
         isLoadingBusStationSeqList ||
         isLoadingBussArriveList ? (
-          <div className="animate-spin w-10 h-10 rounded-full border-t-2 border-blue-500"></div>
+          <div className="flex justify-center items-center py-10">
+            <div className="animate-spin w-10 h-10 rounded-full border-t-2 border-blue-500"></div>
+          </div>
         ) : (
           <BusArriveList busArriveList={busArriveList} />
         )}
